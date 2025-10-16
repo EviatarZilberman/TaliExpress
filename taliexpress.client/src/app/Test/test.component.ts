33bdc125d61d7b9ee59004a1020e418f6ce5f960
Eviatar Zilberman <esztbk@gmail.com>
2025-10-16 23:38:59 +0300

    Test keeps on updating as the study.

--- taliexpress.client/src/app/Test/test.component.ts
+++ taliexpress.client/src/app/Test/test.component.ts
@@ -1,22 +1,62 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+interface Pokemon {
+  id: number,
+  name: string,
+  type: string
+}
+
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css'],
   standalone: false
 })
+
 export class TestComponent {
   var1: number = 1;
   var2: number = 2;
   title: string = 'TestComponent';
   imageUrl: string = 'https://ae01.alicdn.com/kf/Sd2e8f3f3e8f14b1fa6b3f2a1c6c1e8e4D/LED-Strip-Lights-5M-10M-15M-20M-30M-RGB-5050-SMD-Flexible-LED-Light-Strip-Non-waterproof.jpg_Q90.jpg_.webp';
   favoriteAnimal: string = 'Turtle'
+  pokemonName: string = '';
+  pokemons: Pokemon[] = [
+    {
+      id: 1,
+      name: 'Bulbasaur',
+      type: 'Grass/Poison'
+    },
+    {
+      id: 2,
+      name: 'Charmander',
+      type: 'Fire'
+    },
+    {
+      id: 3,
+      name: 'Squirtle',
+      type: 'Water'
+    },
+    {
+      id: 4,
+      name: 'Pikachu',
+      type: 'Electric'
+    },
+    {
+      id: 5,
+      name: 'Eevee',
+      type: 'Normal'
+    }]
 
 
   constructor() {
   }
 
-  
+  public handleClick(pokemonName: string): void {
+    alert(pokemonName);
+  }
+
+  public handleChange(event: any): void {
+    this.pokemonName = event.target.value;
+  }
 }
